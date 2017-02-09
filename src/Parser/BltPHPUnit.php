fa69a17ad759410d8c527e8c6cef700c63dbc299
Kris <kris@meridian-ds.com>
2017-02-09 16:01:19 -0600

    Adding a file exists check to prevent phpunit from running unnecessarily

--- src/Parser/BltPHPUnit.php
+++ src/Parser/BltPHPUnit.php
@@ -16,7 +16,7 @@ class BltPHPUnit extends BltBaseParser {
    * {@inheritdoc}
    */
   public function parse(GitPostReceiveHandler $handler, CommandStackInterface $commandStack) {
-    if ($this->doParse($handler)) {
+    if ($this->doParse($handler) && $this->getFileSystem()->exists($this->getDataDirectoryPath() . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'phpunit')) {
       $commandStack->addCommand("cd {$this->getDataDirectoryPath()}");
       $commandStack->addCommand("./vendor/bin/phpunit");
       $commandStack->execute();
